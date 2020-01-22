332bf4f882ecc387880c7f45debdc90cbfba4b2c
Jemy Chen <jemychen@live.com>
2020-01-22 04:34:01 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,7 +19,6 @@ def literal_aoh
   characters = [
     { name:  "Don Gately", occupation: "Live-in Staff" },{ name:  "Joelle van Dyne", occupation: "Radio Personality" }
     ,{ name:  "Pat Monteseian", occupation: "Staff" }, { name:  "Kate Gompert", occupation: "None" }, { name:  "Bruce Green", occupation: "Fan of Mildred" }]
-  characters.sort
 end
 
 def aoh_lookup(aoh, row, key)
